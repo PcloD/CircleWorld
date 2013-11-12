81a08190e419fca9485f7dcfffbec31fd1b2a1e3
Federico D'Angelo <federicodangelo@gmail.com>
2013-11-11 22:42:30 -0300

    Added alignment options to SpriteView

--- Assets/SourceCode/SpriteMeshEngine/SpriteView.cs
+++ Assets/SourceCode/SpriteMeshEngine/SpriteView.cs
@@ -1,66 +1,95 @@
 using UnityEngine;
 using SpriteMeshEngine;
+using GUIEngine;
 
 [ExecuteInEditMode]
+[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
 public class SpriteView : MonoBehaviour
 {
     public string spriteSheetId;
     public string spriteId;
+    
+    public GUIAlignVertical verticalAlignment;
+    public GUIAlignHorizontal horizontalAlignment;
 
     private SpriteMesh spriteMesh;
     
+#if UNITY_EDITOR
     public void Update()
     {
-        if (!string.IsNullOrEmpty(spriteSheetId) && !string.IsNullOrEmpty(spriteId))
-        {
-            SpriteSheet spriteSheet = SpriteSheetManager.GetSpriteSheet(spriteSheetId);
-            
-            if (spriteSheet != null)
-            {
-                Sprite sprite = spriteSheet.GetSprite(spriteId);
-                
-                if (sprite != null)
-                {
-                    UpdateMesh(sprite);
-                }
-                else
-                {
-                    if (renderer)
-                        renderer.enabled = false;
-                }
-            }
-            else
-            {
-                if (renderer)
-                    renderer.enabled = false;
-            }
-        }
-        else
-        {
-            if (renderer)
-                renderer.enabled = false;
-        }
+        if (!Application.isPlaying)
+            UpdateMesh();
+    }
+#endif
+    
+    public void Start()
+    {
+        UpdateMesh();
     }
     
-    private void UpdateMesh(Sprite sprite)
+    public void UpdateMesh()
     {
+        renderer.enabled = false;
+        
+        if (string.IsNullOrEmpty(spriteSheetId))
+            return;
+            
+        if (string.IsNullOrEmpty(spriteId))
+            return;
+            
+        SpriteSheet spriteSheet = SpriteSheetManager.GetSpriteSheet(spriteSheetId);
+            
+        if (spriteSheet == null)
+            return;
+        
+        Sprite sprite = spriteSheet.GetSprite(spriteId);
+        
+        if (sprite == null)
+            return;
+        
         if (spriteMesh == null)
             spriteMesh = new SpriteMesh();
 
-        if (!GetComponent<MeshRenderer>())
-            gameObject.AddComponent<MeshRenderer>();
-        
-        if (!GetComponent<MeshFilter>())
-            gameObject.AddComponent<MeshFilter>();
-        
         renderer.sharedMaterial = sprite.SpriteSheet.DefaultMaterial;
         renderer.enabled = true;
+        
+        float sizeX = sprite.SizeX;
+        float sizeY = sprite.SizeY;
+        
+        float x = 0;
+        float y = 0;
+        
+        switch(verticalAlignment)
+        {
+            case GUIAlignVertical.Top:
+                y = 0;
+                break;
+            case GUIAlignVertical.Center:
+                y = -sizeY / 2.0f;
+                break;
+            case GUIAlignVertical.Bottom:
+                y = -sizeY;
+                break;
+        }
 
+        switch(horizontalAlignment)
+        {
+            case GUIAlignHorizontal.Left:
+                x = 0;
+                break;
+            case GUIAlignHorizontal.Center:
+                x = -sizeX / 2.0f;
+                break;
+            case GUIAlignHorizontal.Right:
+                x = -sizeX;
+                break;
+        }
+        
         spriteMesh.BeginCalculateSize();
-        spriteMesh.AddSprite(sprite, 0, -sprite.SizeY, sprite.SizeX, sprite.SizeY, Color.white);
+        spriteMesh.AddSprite(sprite, x, y, sizeX, sizeY, Color.white);
         SpriteMeshInfo meshInfo = spriteMesh.End();
         spriteMesh.Begin(meshInfo);
-        spriteMesh.AddSprite(sprite, 0, -sprite.SizeY, sprite.SizeX, sprite.SizeY, Color.white);
+        spriteMesh.AddSprite(sprite, x, y, sizeX, sizeY, Color.white);
         spriteMesh.End();
         spriteMesh.Apply();
 
